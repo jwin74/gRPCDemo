33fb29c7ccb339d241665460dc7079ff3549af2f
Jasper Windahl Andersen <jwin74@users.noreply.github.com>
2021-03-09 22:36:51 +0100

    Updated Main with gRPC Client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+// ReSharper disable UnusedParameter.Local
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var studentClient = new Student.StudentClient(channel);
 
+            // Single Student
+            Console.WriteLine("Single Record Demo");
+            var input = new StudentModelLookup {StudentId = 123456};
+            var student = await studentClient.GetStudentInfoAsync(input);
+
+            Console.WriteLine($"Student Name: {student.FirstName} {student.LastName}, Student ID Number: {student.StudentIdNumber}");
+
+            Console.ReadKey();
         }
     }
 }
